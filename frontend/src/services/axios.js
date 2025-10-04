982ae5a066ed7b4b700ad4e14a22f6dc0200f1cb
ibirothe <132349052+ibirothe@users.noreply.github.com>
2025-10-05 00:24:24 +0200

    Update axios.js

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://jay-two-door-backend-l7ete9v77-ibirothes-projects.vercel.app/api/v1/";
+const baseURL = "https://jay-two-door-backend.vercel.app/api/v1";
 
 const axiosInstance = axios.create({
   baseURL,
